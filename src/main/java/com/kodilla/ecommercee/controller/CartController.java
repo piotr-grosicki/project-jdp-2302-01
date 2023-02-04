a024d27b68e4b85bbd79be7feac412ed085b5daa
Milosz <milosz.sierakowski@wp.pl>
2023-02-04 21:29:51 +0100

    JDP230201-3 Added CartController endpoints and necessary domain class

--- src/main/java/com/kodilla/ecommercee/controller/CartController.java
+++ src/main/java/com/kodilla/ecommercee/controller/CartController.java
@@ -1,4 +1,40 @@
 package com.kodilla.ecommercee.controller;
 
+import com.kodilla.ecommercee.domain.CartsDto;
+import com.kodilla.ecommercee.domain.OrderDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import com.kodilla.ecommercee.domain.ProductInCartDto;
+import org.springframework.data.domain.jaxb.SpringDataJaxb;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CartController {
+
+    @PostMapping
+    public void createCarts(@RequestBody CartsDto cartsDto) {
+        //zrobione utworzenie pustego koszyka
+    }
+
+    @GetMapping
+    public List<ProductDto> getProductsFromCart() {
+        return new ArrayList<>();
+    }
+
+    @PutMapping
+    public ProductInCartDto updateProduct(@RequestBody ProductInCartDto productInCartDto) {
+        return new ProductInCartDto(1, 1);
+    }
+
+    @DeleteMapping
+    public void deleteProductFromCart(@RequestBody ProductInCartDto productInCartDto) {
+    }
+
+    @PostMapping
+    public void createNewOrder(@RequestBody OrderDto orderDto) {
+    }
+
+
 }
