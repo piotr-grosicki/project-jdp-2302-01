a88f70d652dcc611626bc999c95d8f8dd115f4de
mk <kosakowski.m@gmail.com>
2023-02-05 17:39:41 +0100

    Encja Product - implementacja

--- src/main/java/com/kodilla/ecommercee/domain/Product.java
+++ src/main/java/com/kodilla/ecommercee/domain/Product.java
@@ -1,11 +1,33 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name = "products")
 public class Product {
 
-    private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID", unique = true)
+    private Integer id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "PRICE")
     private BigDecimal price;
+
+    @Column(name = "DESCRIPTION")
     private String description;
+
+    @ManyToOne
+    @JoinColumn(name = "GROUP_PRODUCT")
+    private GroupProduct groupProduct;
 }
