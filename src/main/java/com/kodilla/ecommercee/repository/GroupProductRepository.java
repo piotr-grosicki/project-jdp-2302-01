545fc994027d52fbfee0d99c6b2588ddc829793b
Tomasz Kowacki <tomek.kowacki@gmail.com>
2023-02-06 20:28:50 +0100

    Implementacja encji GroupProduct i jej repozytorium

---
+++ src/main/java/com/kodilla/ecommercee/repository/GroupProductRepository.java
@@ -0,0 +1,7 @@
+package com.kodilla.ecommercee.repository;
+
+import com.kodilla.ecommercee.domain.GroupProduct;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GroupProductRepository extends CrudRepository<GroupProduct, Long> {
+}
