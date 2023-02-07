9db820286a3bae30034e785122df4b98f041505a
Milosz <milosz.sierakowski@wp.pl>
2023-02-07 20:07:41 +0100

    JDP230201-3 Repair checks fail v2

--- src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/OrderDto.java
@@ -1,4 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class OrderDto {
+
+    private Long id;
+    private LocalDateTime orderTime;
+    private BigDecimal totalPrice;
+    private boolean status;
 }
