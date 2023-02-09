b43e28e2e3abbafa365ba93188fee5b70ef003b1
mk <kosakowski.m@gmail.com>
2023-02-09 18:28:26 +0100

    Bug w UserController - implementacja klasy KeyDto

--- src/main/java/com/kodilla/ecommercee/domain/KeyDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/KeyDto.java
@@ -1,4 +1,13 @@
 package com.kodilla.ecommercee.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class KeyDto {
+    private LocalDateTime created;
+    private Long key;
 }
