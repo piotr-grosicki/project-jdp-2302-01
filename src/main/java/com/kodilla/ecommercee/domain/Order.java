0218cb73b893ec81cdb158fcd7c0328be437e95f
mk <kosakowski.m@gmail.com>
2023-02-09 17:45:02 +0100

    Dodanie wstepnej implementacji encji Order

--- src/main/java/com/kodilla/ecommercee/domain/Order.java
+++ src/main/java/com/kodilla/ecommercee/domain/Order.java
@@ -1,10 +1,14 @@
 package com.kodilla.ecommercee.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Entity(name = "ORDERS")
 public class Order {
 
+    @Id
     private Long id;
     private LocalDateTime orderTime;
     private BigDecimal totalPrice;
